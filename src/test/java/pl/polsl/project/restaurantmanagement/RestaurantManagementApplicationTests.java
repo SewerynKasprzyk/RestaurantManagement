e8c8011ceeed89e00f9141da5d2ccb32b7cf1011
SewerynKasprzyk <sewkas.studies@gmail.com>
2024-03-18 23:27:04 +0100

    Renew Configuration

---
+++ src/test/java/pl/polsl/project/restaurantmanagement/RestaurantManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.project.restaurantmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
