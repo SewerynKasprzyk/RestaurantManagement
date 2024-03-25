0c8df1e2431492ab07b3fba5a8986ab6bd6584cd
SewerynKasprzyk <sewkas.studies@gmail.com>
2024-03-25 20:55:24 +0100

    Database created standard repository classes.

--- src/main/java/pl/polsl/project/restaurantmanagement/repositories/TableRepository.java
+++ src/main/java/pl/polsl/project/restaurantmanagement/repositories/TableRepository.java
@@ -3,5 +3,5 @@ package pl.polsl.project.restaurantmanagement.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.polsl.project.restaurantmanagement.model.Table;
 
-public interface TableRepository extends JpaRepository<Table, Integer> {
+public interface TableRepository extends JpaRepository<Table_entity, Integer> {
 }
