5ee627c13b3d8a0a0c31306e2c82b75c9fd6ba00
Jakub Dudek <j.dudek294@gmail.com>
2024-03-25 21:00:52 +0100

    Set services

--- src/main/java/pl/polsl/project/restaurantmanagement/service/TimeScheduleService.java
+++ src/main/java/pl/polsl/project/restaurantmanagement/service/TimeScheduleService.java
@@ -1,4 +1,18 @@
 package pl.polsl.project.restaurantmanagement.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.polsl.project.restaurantmanagement.repositories.TimeScheduleRepository;
+
+@Service
 public class TimeScheduleService {
+
+    private final TimeScheduleRepository timeScheduleRepository;
+
+    @Autowired
+    public TimeScheduleService(TimeScheduleRepository timeScheduleRepository) {
+        this.timeScheduleRepository = timeScheduleRepository;
+    }
+
+    // Add your service methods here
 }
