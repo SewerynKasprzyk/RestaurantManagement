eac603a3c11254290cc32588de7f07adff86de61
SewerynKasprzyk <sewkas.studies@gmail.com>
2024-05-06 18:02:32 +0200

    DB:Adding example users method.

--- src/main/java/pl/polsl/project/restaurantmanagement/services/UserService.java
+++ src/main/java/pl/polsl/project/restaurantmanagement/services/UserService.java
@@ -1,10 +1,13 @@
 package pl.polsl.project.restaurantmanagement.services;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.polsl.project.restaurantmanagement.model.User;
+import pl.polsl.project.restaurantmanagement.model.UserType;
 import pl.polsl.project.restaurantmanagement.repositories.UserRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -32,5 +35,24 @@ public class UserService {
     public void deleteUser(Integer id) {
         userRepository.deleteById(id);
     }
+
+    @PostConstruct
+    public void initializeExampleUsers() {
+        List<User> exampleUsers = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            User user = new User();
+            user.setUserType(UserType.CUSTOMER);
+            user.setLogin("user" + i);
+            user.setPassword("password" + i);
+            user.setName("User " + i);
+            user.setSurname("Surname " + i);
+            user.setPhoneNumber("123456789" + i);
+            user.setVerified(true);
+            user.setActive(true);
+            exampleUsers.add(user);
+        }
+        userRepository.saveAll(exampleUsers);
+    }
+
 }
 
