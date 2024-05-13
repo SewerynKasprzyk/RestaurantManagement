bd1c651279e8dff57ed302a3c2724c98d1cc98e8
SewerynKasprzyk <sewkas.studies@gmail.com>
2024-05-13 23:47:40 +0200

    Front: Moving test pages from App.js to .js files

--- src/main/java/pl/polsl/project/restaurantmanagement/configuration/WebConfig.java
+++ src/main/java/pl/polsl/project/restaurantmanagement/configuration/WebConfig.java
@@ -1,24 +1,21 @@
 package pl.polsl.project.restaurantmanagement.configuration;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
-    public FilterRegistrationBean corsFilter(){
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
@@ -34,9 +31,14 @@ public class WebConfig {
                 HttpMethod.DELETE.name()
         ));
         config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-        bean.setOrder(-102);
-        return bean;
+        return source;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
     }
 }
