388375a7905e7b250f037bd62bb2738ef7b12a6a
SewerynKasprzyk <sewkas.studies@gmail.com>
2024-05-30 14:06:29 +0200

    Deleting old config

--- src/main/java/pl/polsl/project/restaurantmanagement/configuration/WebConfig.java
+++ src/main/java/pl/polsl/project/restaurantmanagement/configuration/WebConfig.java
@@ -1,6 +1,5 @@
 package pl.polsl.project.restaurantmanagement.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Add your frontend origin here
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
